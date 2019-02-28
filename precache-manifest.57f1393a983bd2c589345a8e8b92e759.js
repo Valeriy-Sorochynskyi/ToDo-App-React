938571d19081b5fc540a31206a4466caa8c22778
Valeriy-Sorochynkyi <haribol162@gmail.com>
2019-02-28 16:14:30 +0200

    Updates

---
+++ precache-manifest.57f1393a983bd2c589345a8e8b92e759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9449fe3ca299c7668a38",
+    "url": "/todo-app-react/static/css/main.18b59d76.chunk.css"
+  },
+  {
+    "revision": "9449fe3ca299c7668a38",
+    "url": "/todo-app-react/static/js/main.9449fe3c.chunk.js"
+  },
+  {
+    "revision": "71507c0fe40142d1c314",
+    "url": "/todo-app-react/static/js/runtime~main.71507c0f.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/todo-app-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a17649224eb4245e773a683d2f46987",
+    "url": "/todo-app-react/index.html"
+  }
+];
